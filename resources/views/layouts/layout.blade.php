3809578372e5648633117e3c01cacbab4e0e113c
TomGieben <t.gieben@outlook.com>
2022-02-28 11:28:44 +0100

    icon added

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ url('storage/icons/mercurie-logo.png') }}" type="image/icon type">
+    <link rel="icon" href="{{url('/storage/img/icon.png')}}" type="image/icon type">
     {{-- font-awesome --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     {{-- styles --}}
