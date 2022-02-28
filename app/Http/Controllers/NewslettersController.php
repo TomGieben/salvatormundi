d6166baebd9bcb1193909e896adeff5b1520d696
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-02-28 15:46:33 +0100

    Dit

--- app/Http/Controllers/NewslettersController.php
+++ app/Http/Controllers/NewslettersController.php
@@ -17,19 +17,16 @@ class NewslettersController extends Controller
     }
 
     public function store(Request $request) {
+        
         $slug = Str::slug($request->title);
-        // $newsarticle = Newsletters::create([
-        //     'slug' => $slug,
-        //     'title' => $request->title,
-        //     'file' => $file,
-        // ]);
-        dd($slug);
-        // if(Newsletters::where('slug', $slug)->exists()) {
-        //     return redirect()->back()->with('error', 'Er is al een gelijknamig artikel.');
-        // } else {
-            
-        //     // return redirect()->route('admin.newsarticles.edit', $newsarticle->slug);
-        // }
+        $file = $request->file('file');
+        $filename = $slug.'.'.pathinfo($file->getClientOriginalName() , PATHINFO_EXTENSION);
+        $path = $file->storeAs('public/newsletters', $filename);
+        $newsarticle = Newsletters::create([
+            'slug' => $slug,
+            'title' => $request->title,
+            'file' => $filename,
+        ]);
     }
 
     public function update() {
@@ -38,5 +35,9 @@ class NewslettersController extends Controller
 
     public function delete() {
         
+    }
+
+    private function download($file){
+        return response()->download(storage_path('/storage/app/files/'.$file));
     }
 }
