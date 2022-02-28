3a0230ac580b25be727f6e64503bf4c39fba7627
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-02-28 11:29:44 +0100

    NavBurger

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="icon" href="{{ url('storage/icons/mercurie-logo.png') }}" type="image/icon type">
     {{-- font-awesome --}}
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     {{-- styles --}}
@@ -16,16 +15,16 @@
         @include('layouts.nav')
     </header>
     
-
     <div>
         @yield('content')
     </div>
 
-    <div>
+    <footer>
         @include('layouts.footer')
-    </div>
+    </footer>
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
+
 
