d30f91ced799dde198d87a0bfbf155c413ccad42
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-02-28 16:54:27 +0100

    Title fix 2

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
     <title>
-        Salvator-mundi
+        Gilde Salvator Mundi
         @if(!Route::is('welcome.index')) 
         | {{ basename(url()->current()) }}
         @endif
