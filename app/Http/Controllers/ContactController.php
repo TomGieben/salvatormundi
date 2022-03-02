b33f646193dfa8a63eac24bb7612006ca9ccade4
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-03-02 16:06:43 +0100

    Contact ding

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -2,15 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CalendarItems;
+use App\Models\NewsArticles;
 use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
     public function index(){
-        return view('contact.index');
+        $calenderitems = CalendarItems::all();
+        $newsarticles = NewsArticles::all();
+        if(count($newsarticles) > 0) {
+            $newsarticle = $newsarticles->random(1)->first();
+        } else {
+            $newsarticle = null;
+        }
+
+        return view('contact.index', [
+            'calenderitems' => $calenderitems,
+            'newsarticle' => $newsarticle,
+        ]);
     }
 
     public function sendmail(Request $request){
-        dd($request);
+        if($this->spam($request->description)){
+            //yes
+            if(!filter_var($request->email, FILTER_VALIDATE_EMAIL)){
+                return redirect()->back()->with('error', ''.$request->email.' is geen geldige email.')->withInput($request->input());
+            }
+        } else {
+            //neeeee
+        }
+        // dd($request);
+        return redirect()->back()->with('success', 'Hallo '.$request->name.' We hebben jouw mail verstuurt en zullen zo snel mogelijk volgend jaar reageren');
+    }
+
+    public function spam($string) {
+        if(strlen($string) < 25 || strlen($string) > 1500) {
+            return true;
+        } else {
+            return false;
+        }
+
+        if(str_contains($string, ' ')) {
+            return true;
+        } else {
+            return false;
+        }
+
+        return true;
     }
 }
--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -5,6 +5,18 @@
         <hr>
     </div>
     <div class="container">
+        @if ($message = Session::get('error'))
+        <div class="alert-message alert alert-danger col-12 p-2">
+            <p>{{ $message }}</p>
+        </div>
+        @endif
+    
+        @if ($message = Session::get('success'))
+            <div class="alert-message alert alert-success col-12 p-2">
+                <p>{{ $message }}</p>
+            </div>
+            <hr>
+        @endif
         <div class="form">
             <form id="contact-form" name="contact-form" action="{{route('contact.sendmail')}}" method="POST">
                 @csrf
@@ -19,7 +31,7 @@
                     <div class="col-md-6">
                         <div class="md-form mb-0">
                             <label for="email" class="m-1">Jouw email</label>
-                            <input type="text" id="email" name="email" class="form-control" value="" required="">
+                            <input type="email" id="email" name="email" class="form-control" value="" required="">
                         </div>
                     </div>
                 </div>
@@ -47,7 +59,24 @@
             </form>
         </div>
     </div>
-    <div class="container-fluid py-md-5 py-3 mt-4 bg-primary">
-        wejow
+    <div class="container">
+        <div class="row justify-content-between my-4">
+            <div class="col-md-5 col-12 align-self-center my-4">
+                <h2 class="h1">{{$newsarticle->title}}</h2>
+                <p class="h5 font-weight-light">
+                    @if(strlen($newsarticle->description) > 500)
+                        {!! substr($newsarticle->description,0,strpos($newsarticle->description, ' ',800)) !!}
+                    @else
+                        {!! $newsarticle->description !!}
+                    @endif
+                    <a href="{{route('newsarticles.show', $newsarticle->slug)}}" class="btn btn-link text-primary p-1 m-0">
+                        Meer lezen <i class="fa fa-arrow-right"></i>
+                    </a>
+                </p>
+            </div>
+            <div class="col-md-6 col-12">
+                <img loading="lazy" src="{{url('/storage/img/newsarticles/'.json_decode($newsarticle->images, true)[0].'')}}" class="img-fluid rounded shadow-lg mt-2" width="685px" height="527px">
+            </div>
+        </div>
     </div>
 @endsection
