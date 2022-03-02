832bde90a74ca112d7b267b7ef92e6485a415626
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-03-02 14:15:43 +0100

    ugtc

--- app/Http/Controllers/AboutusController.php
+++ app/Http/Controllers/AboutusController.php
@@ -3,10 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PhotoGalleryItems;
 
 class AboutusController extends Controller
 {
     public function index(){
-        return view('aboutus.index');
+        $images = PhotoGalleryItems::all();
+        $rand = rand(0, ($images->count()-1));
+        if(count($images) > 0) {
+            $images = $images->random(5)->toArray();
+        } else {
+            $images = null;
+        }
+        return view('aboutus.index', [
+            'images' => $images,
+            'rand' => $rand,
+        ]);
     }
 }
