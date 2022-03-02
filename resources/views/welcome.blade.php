1dc382a65bfd9cb6a381626735def3ff6ae0d5aa
TomGieben <t.gieben@outlook.com>
2022-03-02 12:04:40 +0100

    slide show

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,13 @@
 @extends('layouts.layout')
 @section('content')
-<div style="background-image: url('{{url('/storage/img/background.png')}}');" class="background-image">
+<div class="background-image" id="carousel">
   <div class="container" style="height: 100vh;">
       <div class="row">
           <div class="col-12 text-center py-5">
-              <h1 class="h1 font-weight-bold text-dark">Gilde Salvator Mundi</h1>
-              <div class="content">
+            <div class="title-blur">
+              <h1 class="h1 font-weight-bold text-white title">Gilde Salvator Mundi</h1>
+            </div>
+              <div class="content" onclick="window.scroll({top:1000, left:0, behavior:'smooth'})">
                 <svg id="more-arrows">
                   <polygon class="arrow-top" points="37.6,27.9 1.8,1.3 3.3,0 37.6,25.3 71.9,0 73.7,1.3 "/>
                   <polygon class="arrow-middle" points="37.6,45.8 0.8,18.7 4.4,16.4 37.6,41.2 71.2,16.4 74.5,18.7 "/>
@@ -34,4 +36,22 @@
       });
   </script>
 @endif
+<script>
+  const images = [
+    "{{url('/storage/img/photogallery/'.$images[0]['image'].'')}}",
+    "{{url('/storage/img/photogallery/'.$images[1]['image'].'')}}",
+    "{{url('/storage/img/photogallery/'.$images[2]['image'].'')}}",
+    "{{url('/storage/img/photogallery/'.$images[3]['image'].'')}}",
+    "{{url('/storage/img/photogallery/'.$images[4]['image'].'')}}",
+  ];
+
+  slide()
+  setInterval(slide, 5000);
+
+  function slide() {
+    var rand = Math.floor(Math.random() * 4);
+    var element = document.getElementById('carousel');
+    element.style = "background-image: url('"+images[rand]+"');";
+  }
+</script>
 @endsection
