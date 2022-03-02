e3c17562d74988c7640fe31ea16495e52ed44a3b
TomGieben <t.gieben@outlook.com>
2022-03-02 15:46:40 +0100

    calender added

--- app/Http/Controllers/CalenderItemsController.php
+++ app/Http/Controllers/CalenderItemsController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CalendarItems;
+use App\Models\NewsArticles;
 use Illuminate\Http\Request;
 
 class CalenderItemsController extends Controller
 {
-    //
+    public function index() {
+        $calenderitems = CalendarItems::all();
+        $newsarticles = NewsArticles::all();
+        if(count($newsarticles) > 0) {
+            $newsarticle = $newsarticles->random(1)->first();
+        } else {
+            $newsarticle = null;
+        }
+
+        return view('calenderitems.index',[
+            'calenderitems' => $calenderitems,
+            'newsarticle' => $newsarticle,
+        ]);
+    }
 }
