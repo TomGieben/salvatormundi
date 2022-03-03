4394c4e18e8491f3b0c13ce307f863830c5a6d67
Tino van de Kolk <tino.kolk@rocdeleijgraaf.nl>
2022-03-03 16:50:36 +0100

    fix

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,14 +2,58 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Visits;
+use App\Models\User;
+use App\Models\PhotoGalleryCategories;
+use App\Models\PhotoGalleryItems;
+use App\Models\Newsletters;
+use App\Models\NewsArticles;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('admin.dashboard.index',[
+        $categories = PhotoGalleryCategories::count();
+        $images = PhotoGalleryItems::count();
+        $newsLetters = Newsletters::count() - 1;
+        $users = User::all();
+        $newsArticles = NewsArticles::count();
 
+        $visitsTimes = [
+            'Vandaag' => Visits::whereDay('updated_at', '=', date('d'))->get(),
+            'Deze maand' => Visits::whereMonth('updated_at', '=', date('m'))->get(),
+            'Afgelopen jaar' => Visits::whereYear('updated_at', '=', date('Y'))->get(),
+            'Totaal' => Visits::all(),
+        ];
+
+        $pages = [];
+
+        foreach($visitsTimes as $key=>$time){
+            foreach($time as $visit) {
+                $page = parse_url($visit->page);
+                if(!isset($page['path'])) {
+                    $page['path'] = 'home';
+                }
+                if(str_contains($page['path'], '/')){
+                    $page['path'] = str_replace('/', '', $page['path']);
+                }
+
+                if(in_array($page['path'], $pages)) {
+                    $pages[$key][$page['path']]['views'] = $pages[$key][$page['path']]['views']++;
+                } else {
+                    $pages[$key][$page['path']]['views'] = 1;
+                }
+            }
+        }
+
+        return view('admin.dashboard.index',[
+            'visits' => $pages,
+            'users' => $users,
+            'imgCount' => $images,
+            'imgCatCount' => $categories,
+            'newsLetterTotal' => $newsLetters,
+            'newsArticles' => $newsArticles,
         ]);
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,18 +1,198 @@
 @extends('admin.layouts.layout')
 @section('content')
     <div class="container">
-        
+        <div class="row col-auto m-0 px-0">
+            <h1 class="my-4">Gilde Salvator Mundi | Dashboard</h1>
+            <div class="text-right my-4 col px-0">
+                <a href="{{route('welcome.index')}}" class="btn btn-success my-2">Website <i class="fa fa-eye"></i></a>
+            </div>
+        </div>
+        <hr class="mt-0">
     </div>
-    <div class="container">
-        <div class="row">
+    @if(auth()->user()->admin == 1)
+        <div class="container">
             <div class="card">
                 <div class="card-header">
-                    
+                    <div class="h4">
+                        <a href="{{route('admin.users.index')}}">Gebruikers</a>
+                    </div>
+                    <div class="h6">
+                        Er zijn <u>{{$users->count()}}</u> Administratoren en schrijvers
+                    </div>
                 </div>
                 <div class="card-body">
-
+                    <ul class="list-group" id="list">
+                        @if($users)
+                            @foreach ($users as $user)
+                                <li class="list-group-item">
+                                    Naam: <b>{{$user->name}}</b> |
+                                    Email: <b>{{$user->email}}</b> |
+                                    Status: 
+                                    @if($user->admin) <span class="text-danger">Administrator</span> @else <span class="text-info">Schrijver</span> @endif |
+                                </li>
+                            @endforeach
+                        @endif
+                    </ul>
+                </div>
+            </div>
+            <hr>
+        </div>
+    @endif
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                <div class="h4">
+                    Bezoekjes
+                </div>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    @foreach ($visits as $key=>$visit)
+                        <div class="card my-2 mx-auto">
+                            <div class="card-header">
+                                <h4 class="text-center">{{$key}}</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        Pagina
+                                    </div>
+                                    <div class="col text-right">
+                                        Aantal keer bekeken
+                                    </div>
+                                </div>
+                                @foreach($visit as $key=>$value)
+                                    <div class="row">
+                                        <div class="col">
+                                            {{$key}}
+                                        </div>
+                                        <div class="col text-right">
+                                            {{$value['views']}}
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             </div>
         </div>
+    </div>
+    <div class="container">
+        <hr>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="h4">
+                            <a href="{{route('admin.photogallery.index')}}">Foto Gallerij</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                Totaal aantal foto's:
+                            </div>
+                            <div class="col text-right">
+                                {{$imgCount}}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                Totaal aantal Categorieën: 
+                            </div>
+                            <div class="col text-right">
+                                {{$imgCatCount}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="h4">
+                            <a href="{{route('admin.newsletters.index')}}">Nieuws brieven</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                Niews brieven:
+                            </div>
+                            <div class="col text-right">
+                                {{$newsLetterTotal}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <hr>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="h4">
+                            <a href="{{route('admin.newsarticles.index')}}">Nieuws artikelen</a>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                Aantal: 
+                            </div>
+                            <div class="col text-right">
+                                {{$newsArticles}}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="h4">
+                            Account
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col">
+                                Gebruikers naam: 
+                            </div>
+                            <div class="col text-right">
+                                {{auth()->user()->name}}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                Email: 
+                            </div>
+                            <div class="col text-right">
+                                {{auth()->user()->email}}
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                Status: 
+                            </div>
+                            <div class="col text-right">
+                                @if($user->admin) <span class="text-danger">Administrator</span> @else <span class="text-info">Schrijver</span> @endif
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container" style="height: 10vh;">
+
     </div>
 @endsection
